e1e4c1f2a28b843ffd320e5012a66263931f16ad
Walker D. Adams <tsolval@gmail.com>
2014-01-14 16:28:16 -0600

    updated to add getters/setters to Campaign

--- chargen/src/main/java/net/tsolval/chargen/model/Campaign.java
+++ chargen/src/main/java/net/tsolval/chargen/model/Campaign.java
@@ -13,6 +13,51 @@ public class Campaign {
 	private long id;
 	private String slug;
 	private String game;
+
 	// private User gameMaster;
 	// private List<User> players;
+	/**
+	 * @return the id
+	 */
+	public long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the slug
+	 */
+	public String getSlug() {
+		return slug;
+	}
+
+	/**
+	 * @param slug
+	 *            the slug to set
+	 */
+	public void setSlug(String slug) {
+		this.slug = slug;
+	}
+
+	/**
+	 * @return the game
+	 */
+	public String getGame() {
+		return game;
+	}
+
+	/**
+	 * @param game
+	 *            the game to set
+	 */
+	public void setGame(String game) {
+		this.game = game;
+	}
 }
